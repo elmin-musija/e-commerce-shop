d0a40da328b0463395225e7cf367aaa8cf45c439
Mikex95 <mike.beha1337@gmail.com>
2022-12-12 17:43:38 +0100

    add files onboarding page

--- src/pages/Onboarding/Onboarding.js
+++ src/pages/Onboarding/Onboarding.js
@@ -1,9 +1,13 @@
 import "./Onboarding.css";
+import { ReactComponent as OnboardingImage } from '../../img/Onboarding-img.svg'
+import { ReactComponent as OnboardingInnerImage } from '../../img/onboarding-inner-image.svg'
 
 const Onboarding = (props) => {
 	return (
 		<div className="onboarding">
-			<h1>Onboarding</h1>
+			<OnboardingInnerImage className="onboarding__img"/>
+			<h2 className="onboarding__h2">Biggest Sell at Your Fingerprint</h2>
+			<p className="onboarding__p">Find your best products from popular shop wihout any delay</p>
 		</div>
 	);
 };
