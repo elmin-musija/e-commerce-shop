8412d7630807b140ed32c016e3e4d553db2242d6
“NilsNeumann11” <nilstorbenneumann@gmail.com>
2022-12-12 16:23:20 +0100

    add font size

--- src/pages/Splash/Splash.js
+++ src/pages/Splash/Splash.js
@@ -1,13 +1,13 @@
 import "./Splash.css";
 
 const Splash = (props) => {
-	return (
-		<div className="splash">
-			<img src="" alt="" />
-			<h1>e-shop</h1>
-			<h2>Your shopping solution</h2>
-		</div>
-	);
+  return (
+    <div className="splash">
+      <img className="splash__img" src="" alt="" />
+      <h1 className="splash__header">e-shop</h1>
+      <h2 className="splash__subheader">Your shopping solution</h2>
+    </div>
+  );
 };
 
 export default Splash;
