df1175abc6fbcd210789850724637a33c40f945c
Elmin Musija <office.elmin.musija@gmail.com>
2022-12-12 18:03:31 +0100

    update custom button and add to onboarding component

--- src/pages/Onboarding/Onboarding.js
+++ src/pages/Onboarding/Onboarding.js
@@ -1,13 +1,16 @@
+import { ReactComponent as OnboardingInnerImage } from "../../img/onboarding-inner-image.svg";
+import CustomButton from "../../components/CustomButton/CustomButton";
 import "./Onboarding.css";
-import { ReactComponent as OnboardingImage } from '../../img/Onboarding-img.svg'
-import { ReactComponent as OnboardingInnerImage } from '../../img/onboarding-inner-image.svg'
 
 const Onboarding = (props) => {
 	return (
 		<div className="onboarding">
-			<OnboardingInnerImage className="onboarding__img"/>
+			<OnboardingInnerImage className="onboarding__img" />
 			<h2 className="onboarding__h2">Biggest Sell at Your Fingerprint</h2>
-			<p className="onboarding__p">Find your best products from popular shop wihout any delay</p>
+			<p className="onboarding__p">
+				Find your best products from popular shop wihout any delay
+			</p>
+			<CustomButton pr_class="onboarding__button">Get Started</CustomButton>
 		</div>
 	);
 };
