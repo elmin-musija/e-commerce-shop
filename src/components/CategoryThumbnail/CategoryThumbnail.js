dab02416dd94e3db01892732b44ef6d6bf936fae
Elmin Musija <office.elmin.musija@gmail.com>
2022-12-13 16:16:07 +0100

    add fetch to category bar

--- src/components/CategoryThumbnail/CategoryThumbnail.js
+++ src/components/CategoryThumbnail/CategoryThumbnail.js
@@ -1,50 +1,18 @@
 import "./CategoryThumbnail.css";
 
 const CategoryThumbnail = (props) => {
-  return (
-    <div className="category-thumbnail">
-      <div>
-        <img
-          className="category-thumbnail__image"
-          src={props.pr_image}
-          alt={props.pr_alt}
-        />
-        <p className="category-subhead">{props.pr_category}Category 1</p>
-      </div>
-      {/* <div>
-        <img
-          className="category-thumbnail__image"
-          src={props.pr_image}
-          alt={props.pr_alt}
-        />
-        <p className="category-subhead">{props.pr_category}Category 1</p>
-      </div>
-      <div>
-        <img
-          className="category-thumbnail__image"
-          src={props.pr_image}
-          alt={props.pr_alt}
-        />
-        <p className="category-subhead">{props.pr_category}Category 1</p>
-      </div>
-      <div>
-        <img
-          className="category-thumbnail__image"
-          src={props.pr_image}
-          alt={props.pr_alt}
-        />
-        <p className="category-subhead">{props.pr_category}Category 1</p>
-      </div>
-      <div>
-        <img
-          className="category-thumbnail__image"
-          src={props.pr_image}
-          alt={props.pr_alt}
-        />
-        <p className="category-subhead">{props.pr_category}Category 1</p>
-      </div> */}
-    </div>
-  );
+	return (
+		<div className="category-thumbnail">
+			<div>
+				<img
+					className="category-thumbnail__image"
+					src={props.pr_src}
+					alt={props.pr_alt}
+				/>
+				<p className="category-subhead">{props.pr_category}</p>
+			</div>
+		</div>
+	);
 };
 
 export default CategoryThumbnail;
