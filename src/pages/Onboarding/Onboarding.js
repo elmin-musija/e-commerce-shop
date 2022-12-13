8b9d9cb3e5766f6858246c450f56ae8607e3a125
Demiourg <georgios.alexiou@protonmail.com>
2022-12-13 23:01:20 +0100

    add onboarding image

--- src/pages/Onboarding/Onboarding.js
+++ src/pages/Onboarding/Onboarding.js
@@ -4,18 +4,18 @@ import CustomButton from "../../components/CustomButton/CustomButton";
 import "./Onboarding.css";
 
 const Onboarding = (props) => {
-	return (
-		<div className="onboarding">
-			<OnboardingInnerImage className="onboarding__img" />
-			<h2 className="onboarding__h2">Biggest Sell at Your Fingerprint</h2>
-			<p className="onboarding__p">
-				Find your best products from popular shop wihout any delay
-			</p>
-			<Link className="onboarding__link" to="/home">
-				<CustomButton pr_class="onboarding__button">Get Started</CustomButton>
-			</Link>
-		</div>
-	);
+  return (
+    <div className="onboarding">
+      <OnboardingInnerImage className="onboarding__img" />
+      <h2 className="onboarding__h2">Biggest Sell at Your Fingerprint</h2>
+      <p className="onboarding__p">
+        Find your best products from popular shop without any delay
+      </p>
+      <Link className="onboarding__link" to="/home">
+        <CustomButton pr_class="onboarding__button">Get Started</CustomButton>
+      </Link>
+    </div>
+  );
 };
 
 export default Onboarding;
