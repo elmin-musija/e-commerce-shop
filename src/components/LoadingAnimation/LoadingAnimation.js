235cde5f141490eba244166efed5adc51cbd9998
“NilsNeumann11” <nilstorbenneumann@gmail.com>
2022-12-13 15:53:32 +0100

    add splash loading screen animation

---
+++ src/components/LoadingAnimation/LoadingAnimation.js
@@ -0,0 +1,15 @@
+import "./LoadingAnimation.css";
+
+const LoadingAnimation = () => {
+  return (
+    <div className="loading-animation-container">
+      <div className="animation-grid-item"></div>
+      <div className="animation-grid-item"></div>
+      <div className="animation-grid-item"></div>
+      <div className="animation-grid-item"></div>
+      <div className="animation-grid-item"></div>
+    </div>
+  );
+};
+
+export default LoadingAnimation;
--- src/pages/Splash/Splash.js
+++ src/pages/Splash/Splash.js
@@ -4,41 +4,45 @@ import AppContext from "../../context/AppContext";
 import { Navigate } from "react-router-dom";
 import { ReactComponent as SplashIcon } from "../../img/Icon.svg";
 import { ReactComponent as SplashLogo } from "../../img/Logo.svg";
+import LoadingAnimation from "../../components/LoadingAnimation/LoadingAnimation";
 import "./Splash.css";
 
 const Splash = (props) => {
-	const [redirect, setRedirect] = useState(false);
-	const { setFetchAllItems } = useContext(AppContext);
+  const [redirect, setRedirect] = useState(false);
+  const { setFetchAllItems } = useContext(AppContext);
 
-	const fetchFunctionAllItems = () => {
-		fetch("https://dummyjson.com/products/?limit=30")
-			.then((response) => response.json())
-			.then((allItems) => {
-				setFetchAllItems(true, allItems.products);
-			});
-	};
+  const fetchFunctionAllItems = () => {
+    fetch("https://dummyjson.com/products/?limit=30")
+      .then((response) => response.json())
+      .then((allItems) => {
+        setFetchAllItems(true, allItems.products);
+      });
+  };
 
-	useEffect(fetchFunctionAllItems, []);
+  useEffect(fetchFunctionAllItems, []);
 
-	const redirectToOnboarding = () => {
-		return <Navigate to="/onboarding" />;
-	};
+  const redirectToOnboarding = () => {
+    return <Navigate to="/onboarding" />;
+  };
 
-	useEffect(() => {
-		const timer = setTimeout(() => {
-			setRedirect(true);
-		}, 2500);
-		return () => clearTimeout(timer);
-	}, []);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setRedirect(true);
+    }, 2500);
+    return () => clearTimeout(timer);
+  }, []);
 
-	return (
-		<div className="splash">
-			<SplashIcon className="splash__icon" />
-			<SplashLogo className="splash__logo" />
-			<h2 className="splash__subheader">Your shopping solution</h2>
-			{redirect && redirectToOnboarding()}
-		</div>
-	);
+  return (
+    <div className="splash">
+      <SplashIcon className="splash__icon e-shop-animation-one" />
+      <SplashLogo className="splash__logo e-shop-animation-two" />
+      <h2 className="splash__subheader e-shop-animation-three">
+        Your shopping solution
+      </h2>
+      {redirect && redirectToOnboarding()}
+      <LoadingAnimation />
+    </div>
+  );
 };
 
 export default Splash;
