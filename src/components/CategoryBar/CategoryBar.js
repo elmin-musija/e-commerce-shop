88898810ed5d20308fe5a27afbaf6992f072f077
“NilsNeumann11” <nilstorbenneumann@gmail.com>
2022-12-14 16:37:57 +0100

    filter grid error

--- src/components/CategoryBar/CategoryBar.js
+++ src/components/CategoryBar/CategoryBar.js
@@ -4,24 +4,24 @@ import AppContext from "../../context/AppContext";
 import "./CategoryBar.css";
 
 const CategoryBar = (props) => {
-	const { getCategoriesArray } = useContext(AppContext);
+  const { getCategoriesArray } = useContext(AppContext);
 
-	const displayCategoryBar = () => {
-		const tmp = getCategoriesArray();
-		if (tmp.length > 0) {
-			return tmp.map((element) => {
-				return (
-					<CategoryThumbnail
-						pr_src={element[0].thumbnail}
-						pr_alt={element[0].title}
-						pr_category={element[0].category}
-					/>
-				);
-			});
-		}
-	};
+  const displayCategoryBar = () => {
+    const tmp = getCategoriesArray();
+    if (tmp.length > 0) {
+      return tmp.map((element) => {
+        return (
+          <CategoryThumbnail
+            pr_src={element[0].thumbnail}
+            pr_alt={element[0].title}
+            pr_category={element[0].category}
+          />
+        );
+      });
+    }
+  };
 
-	return <div className="category-bar">{displayCategoryBar()}</div>;
+  return <div className="category-bar">{displayCategoryBar()}</div>;
 };
 
 export default CategoryBar;
