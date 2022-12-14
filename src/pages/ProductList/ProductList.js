757e70973c681520c74f3e13fa380c1f949fc047
Elmin Musija <office.elmin.musija@gmail.com>
2022-12-14 09:05:23 +0100

    update footer styling

--- src/pages/ProductList/ProductList.js
+++ src/pages/ProductList/ProductList.js
@@ -28,24 +28,26 @@ const ProductList = (props) => {
 	}, [getUserSearchString()]);
 
 	return (
-		<div className="product-list">
+		<>
 			<SearchBar className="product-list__searchbar" />
-			<p className="product-list__p">Sort by: </p>
-			<div className="product-list-props">
-				{data.map((element, index) => {
-					return (
-						<ProductItem
-							key={index}
-							pr_image={element.images[0]}
-							pr_alt={element.brand}
-							pr_rating={element.rating}
-							pr_price={element.price}
-						/>
-					);
-				})}
+			<div className="product-list">
+				<p className="product-list__p">Sort by: </p>
+				<div className="product-list-props">
+					{data.map((element, index) => {
+						return (
+							<ProductItem
+								key={index}
+								pr_image={element.images[0]}
+								pr_alt={element.brand}
+								pr_rating={element.rating}
+								pr_price={element.price}
+							/>
+						);
+					})}
+				</div>
 			</div>
 			<FooterSticky />
-		</div>
+		</>
 	);
 };
 
