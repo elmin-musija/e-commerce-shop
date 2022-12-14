305b28eb23ff157e5a21764b6f142987404a4670
Mikex95 <mike.beha1337@gmail.com>
2022-12-14 11:56:19 +0100

    feature-home-sort

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,16 +1,27 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import { Navigate } from "react-router-dom";
 import AppContext from "../../context/AppContext";
 import SearchBar from "../../components/SearchBar/SearchBar";
 import CategoryBar from "../../components/CategoryBar/CategoryBar";
 import Footer from "../../components/Footer/Footer";
-import FooterSticky from "../../components/Footer/FooterSticky";
+import ProductItem from "../../components/ProductItem/ProductItem"
 import "./Home.css";
 
 const Home = (props) => {
+	const [data, setData] = useState([])
 	const { getUserSearchString, setUserSearchString } = useContext(AppContext);
 	const { getFetchAllItemsState, getFetchAllItemsData } =
 		useContext(AppContext);
+
+		useEffect(() => {
+				const result = getFetchAllItemsData();
+				result.sort((a, b) => {
+					return b.rating - a.rating;
+					
+				});
+				setData(result);
+		}, []);
+	console.log(data);
 
 	const redirectToProductDetails = () => {
 		if (getUserSearchString() !== "") {
@@ -31,6 +42,11 @@ const Home = (props) => {
 			<h1 className="home__h1">Find your favourite Product</h1>
 			<SearchBar />
 			<CategoryBar />
+			<div className="home__grid">
+				{data.map((elt, index) =>{
+				return <ProductItem key={index} pr_image={elt.images[0]} pr_alt={elt.brand} pr_rating={elt.rating} pr_price={elt.price}/>
+			})}
+			</div>
 			<Footer />
 			{redirectToProductDetails()}
 		</div>
